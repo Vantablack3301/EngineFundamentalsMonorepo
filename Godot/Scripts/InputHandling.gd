55f35fdc9fafcd0b38ab982573b8d0114a6b08b8
Vantablack3301 <dantemorrison@gmail.com>
2026-03-19 16:11:31 -0600

    migrated godot character controller to a custom one

--- Godot/Scripts/InputHandling.gd
+++ Godot/Scripts/InputHandling.gd
@@ -1,7 +1,17 @@
-extends Node
+extends CharacterBody3D
 
 var attack_held = false
 
+var look_rotation : Vector2
+
+var move_speed = 8
+var look_speed = 1
+var jump_velocity = 5
+var collectedTrees = 0
+
+var mouse_captured : bool
+
+@onready var head: Node3D = $Head
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,5 +24,50 @@ func _process(delta: float) -> void:
 		attack_held = true
 		print("Attack")
 		
-	var move := Input.get_vector("Move_Left", "Move_Right", "Move_Backward", "Move_Forward")
-	#print("Move ", move)
+	var move := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backward")
+	var move_dir := (transform.basis * Vector3(move.x, 0, move.y)).normalized()
+	
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = jump_velocity
+	
+	velocity.x = move_dir.x * move_speed
+	velocity.z = move_dir.z * move_speed
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	move_and_slide()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		capture_mouse()
+	
+	if Input.is_key_pressed(KEY_ESCAPE):
+		release_mouse()
+		
+	if mouse_captured and event is InputEventMouseMotion:
+		rotate_look(event.relative)
+	
+func rotate_look(rot_input : Vector2):
+	look_rotation.x -= rot_input.y * look_speed
+	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
+	look_rotation.y -= rot_input.x * look_speed
+	transform.basis = Basis()
+	rotate_y(look_rotation.y)
+	head.transform.basis = Basis()
+	head.rotate_x(look_rotation.x)
+	
+func capture_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+	
+func release_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
+
+
+func _increment_tree_count():
+	collectedTrees += 1
+	print(collectedTrees)
+
+func _get_collected_trees() -> int:
+	return collectedTrees
